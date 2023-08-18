7e050ae913c10f11614950961f79d1a2c1da06d1
soheb-funtune <soheb.sayyad@fyntune.com>
2023-08-18 11:58:09 -0700

    portfolio done

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,23 +1,70 @@
 import React from "react";
 import "./footer.css";
 import { AiTwotoneHeart } from "react-icons/ai";
+import { motion, AnimatePresence } from "framer-motion";
+import { BsGithub, BsLinkedin, BsInstagram } from "react-icons/bs";
+import { MdEmail, MdLocationOn, MdLocationPin } from "react-icons/md";
+import { BiSolidPhone } from "react-icons/bi";
 
 const Footer = () => {
   return (
-    <div className="footer-container">
-      <div className="innter-container">
-        <>
-          <h5 className="heading-text">
-            Contact No : <span>9604376207</span>
-          </h5>
-          <h4 className="heading-text">
-            Email : <span>sohebs5050@gmail.com</span>
-          </h4>
-        </>
+    <div id="contact" className="footer-container">
+      <div className="inner">
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 1, type: "spring", stiffness: 100 }}
+        >
+          <h6 className="contact-text">
+            Contact No : {` `} <span> {` `} 9604376207</span>
+          </h6>
+          <h6 className="contact-text">
+            Email : {` `} <span>{` `} sohebs5050@gmail.com</span>
+          </h6>
+        </motion.div>
+        <AnimatePresence>
+          <motion.div
+            initial={{ x: 100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 1, type: "spring", stiffness: 100 }}
+            className="social-media"
+          >
+            <a target="_blank" href="https://github.com/soheb-funtune">
+              <BsGithub className="icon" />
+            </a>
+            <a
+              target="_blank"
+              href="https://www.linkedin.com/in/soheb-sayyad-a31730225/"
+            >
+              <BsLinkedin className="icon" />
+            </a>
+            <a target="_blank" href="https://www.instagram.com/shoheb8899/">
+              <BsInstagram className="icon" />
+            </a>
+            <a target="_blank" href="mailto:sohebs5050@gmail.com">
+              <MdEmail className="icon" />
+            </a>
+            <a target="_blank" href="tel:+919604376207">
+              <BiSolidPhone className="icon" />
+            </a>
+            <a target="_blank" href="https://github.com/soheb-funtune">
+              <MdLocationPin className="icon" />
+            </a>
+          </motion.div>
+        </AnimatePresence>
       </div>
-      <h2>
-        Made with <AiTwotoneHeart /> By Mohammad Soheb
-      </h2>
+      <motion.h3
+        initial={{ y: 100, opacity: 0 }}
+        whileInView={{ y: 0, opacity: 1 }}
+        transition={{ duration: 1, type: "spring", stiffness: 100 }}
+        className="copy-write"
+      >
+        Made with{" "}
+        <AiTwotoneHeart
+          style={{ color: "red", margin: "0px 10px", fontSize: "30px" }}
+        />{" "}
+        By Mohammad Soheb
+      </motion.h3>
     </div>
   );
 };
